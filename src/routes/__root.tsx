2d890dacbf57043e7daf1c1406c70efc58191b8b
natlus <jesper.sultan@gmail.com>
2025-05-06 18:55:50 +0200

    add a page title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,9 +12,17 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { SunMoon, Wrench } from "lucide-react";
 import { Toaster } from "@/components/ui/sonner";
+import { HeadContent } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: RootLayout,
+  head: () => ({
+    meta: [
+      {
+        title: "mobtimer",
+      },
+    ],
+  }),
 });
 
 function RootLayout() {
@@ -54,46 +62,49 @@ function RootLayout() {
   }, [theme]);
 
   return (
-    <div className="grid grid-rows-[56px_1fr] h-full gap-2 p-2">
-      <header className="flex flex-row gap-6 px-2">
-        <DropdownMenu>
-          <DropdownMenuTrigger className="outline-none hover:scale-[1.2] transition-transform duration-300">
-            <Wrench />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem onClick={() => setMobs([])}>
-              Clear all mobs
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setRecentParticipants([])}>
-              Clear recent participants
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <>
+      <HeadContent />
+      <div className="grid grid-rows-[56px_1fr] h-full gap-2 p-2">
+        <header className="flex flex-row gap-6 px-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="outline-none hover:scale-[1.2] transition-transform duration-300">
+              <Wrench />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onClick={() => setMobs([])}>
+                Clear all mobs
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setRecentParticipants([])}>
+                Clear recent participants
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
 
-        <DropdownMenu>
-          <DropdownMenuTrigger className="outline-none ml-auto hover:scale-[1.2] transition-transform duration-300">
-            <SunMoon />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>
-              System
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </header>
-      <div className="grid md:grid-cols-[1fr_500px_1fr] grid-cols-[1fr_2fr] max-w-full gap-2 h-max self-start pt-16">
-        <Sidebar />
-        <div className="relative flex justify-center items-start">
-          <Outlet />
+          <DropdownMenu>
+            <DropdownMenuTrigger className="outline-none ml-auto hover:scale-[1.2] transition-transform duration-300">
+              <SunMoon />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+        <div className="grid md:grid-cols-[1fr_500px_1fr] grid-cols-[1fr_2fr] max-w-full gap-2 h-max self-start pt-16">
+          <Sidebar />
+          <div className="relative flex justify-center items-start">
+            <Outlet />
+          </div>
         </div>
+        <Toaster position="top-center" />
       </div>
-      <Toaster position="top-center" />
-    </div>
+    </>
   );
 }
